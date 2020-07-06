50a6bdffb915c72d61834ff3ce5036996f53e0e7
Frehzy <55092493+Frehzy@users.noreply.github.com>
2020-07-06 20:41:21 +0500

    Add files via upload

---
+++ SuspendProcess/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SuspendProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ProcessNameLabel = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendButton = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.UpdateListButton = new MetroFramework.Controls.MetroButton();
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            this.ExitButton = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // ProcessNameLabel
+            // 
+            this.ProcessNameLabel.Location = new System.Drawing.Point(125, 60);
+            this.ProcessNameLabel.Name = "ProcessNameLabel";
+            this.ProcessNameLabel.Size = new System.Drawing.Size(199, 23);
+            this.ProcessNameLabel.TabIndex = 2;
+            this.ProcessNameLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // SuspendButton
+            // 
+            this.SuspendButton.Location = new System.Drawing.Point(330, 60);
+            this.SuspendButton.Name = "SuspendButton";
+            this.SuspendButton.Size = new System.Drawing.Size(106, 52);
+            this.SuspendButton.TabIndex = 3;
+            this.SuspendButton.Text = "Suspend Process";
+            this.SuspendButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.SuspendButton.Click += new System.EventHandler(this.SuspendButton_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(96, 20);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Process name";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 89);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(79, 20);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Time: 5000";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(22, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(414, 132);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            // 
+            // UpdateListButton
+            // 
+            this.UpdateListButton.AutoSize = true;
+            this.UpdateListButton.Location = new System.Drawing.Point(23, 258);
+            this.UpdateListButton.Name = "UpdateListButton";
+            this.UpdateListButton.Size = new System.Drawing.Size(86, 27);
+            this.UpdateListButton.TabIndex = 8;
+            this.UpdateListButton.Text = "UpdateList";
+            this.UpdateListButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.UpdateListButton.Click += new System.EventHandler(this.UpdateListButton_Click);
+            // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.Location = new System.Drawing.Point(125, 89);
+            this.metroTrackBar1.Maximum = 20000;
+            this.metroTrackBar1.Minimum = 1;
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(199, 23);
+            this.metroTrackBar1.TabIndex = 9;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTrackBar1.Value = 5000;
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroTrackBar1_Scroll);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.AutoSize = true;
+            this.ExitButton.Location = new System.Drawing.Point(350, 259);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(86, 27);
+            this.ExitButton.TabIndex = 10;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(461, 308);
+            this.ControlBox = false;
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.metroTrackBar1);
+            this.Controls.Add(this.UpdateListButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.SuspendButton);
+            this.Controls.Add(this.ProcessNameLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Suspend Process";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox ProcessNameLabel;
+        private MetroFramework.Controls.MetroButton SuspendButton;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton UpdateListButton;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private MetroFramework.Controls.MetroButton ExitButton;
+    }
+}
+
---
+++ SuspendProcess/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace SuspendProcess
+{
+    public partial class Form1 : MetroForm
+    {
+        public static bool isWork = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+
+        public static void SuspendProcess(int pid)
+        {
+            isWork = true;
+
+            var process = Process.GetProcessById(pid);
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+                SuspendThread(pOpenThread);
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        public static void ResumeProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } 
+                while (suspendCount > 0);
+                CloseHandle(pOpenThread);
+                isWork = false;
+            }
+        }
+
+        private void SuspendButton_Click(object sender, EventArgs e)
+        {
+            var process = Process.GetProcessesByName(ProcessNameLabel.Text)
+                ?.FirstOrDefault(x => x.ProcessName == ProcessNameLabel.Text);
+            if (process == null)
+            {
+                MessageBox.Show(ProcessNameLabel.Text + " process not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                UpdateListButton.PerformClick();
+                return;
+            }
+            var pid = process.Id;
+            SuspendProcess(pid);
+
+            SuspendButton.Enabled = false;
+            ExitButton.Enabled = false;
+            listBox1.Enabled = false;
+            metroTrackBar1.Enabled = false;
+            UpdateListButton.Enabled = false;
+
+            Thread.Sleep(Convert.ToInt32(metroTrackBar1.Value));
+            ResumeProcess(pid);
+
+            SuspendButton.Enabled = true;
+            ExitButton.Enabled = true;
+            listBox1.Enabled = true;
+            metroTrackBar1.Enabled = true;
+            UpdateListButton.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getProcess();
+        }
+
+        public void getProcess()
+        {
+            List<string> arrayProcess = new List<string>();
+            arrayProcess.Clear();
+            listBox1.Items.Clear();
+            foreach (Process winProc in Process.GetProcesses())
+            {
+                arrayProcess.Add(winProc.ProcessName + ": " + winProc.Id);
+            }
+            arrayProcess.Sort();
+            foreach (string item in arrayProcess)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            string text = listBox1.Items[listBox1.SelectedIndex].ToString();
+            var result = text.Split(':')[0];
+            ProcessNameLabel.Text = result.ToString();
+        }
+
+        private void UpdateListButton_Click(object sender, EventArgs e)
+        {
+            getProcess();
+        }
+
+        private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            metroLabel2.Text = "Time: " + metroTrackBar1.Value.ToString();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            try
+            { Application.Exit(); }
+            catch
+            { Close(); }
+        }
+    }
+}
